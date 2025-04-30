dfe9f132e95e6dbb739d9eb9371334876f4e19c1
Romain Deltour <rdeltour@gmail.com>
2025-04-30 06:06:16 +0200

    refactor: use List instead of Set in MetadataSet
    Using Set(s) caused issues with duplicate
    metadata, which did not show up in the parsed
    data structure.

--- src/main/java/com/adobe/epubcheck/opf/OPFChecker30.java
+++ src/main/java/com/adobe/epubcheck/opf/OPFChecker30.java
@@ -22,6 +22,7 @@
 
 package com.adobe.epubcheck.opf;
 
+import java.util.List;
 import java.util.Set;
 
 import org.w3c.epubcheck.core.references.Reference;
@@ -460,7 +461,7 @@ public class OPFChecker30 extends OPFChecker
   {
     MetadataSet metadata = ((OPFHandler30) opfHandler).getMetadata();
     // search total durations metadata expressions
-    Set<Metadata> totalDurationExpressions = metadata
+    List<Metadata> totalDurationExpressions = metadata
         .getPrimary(MediaOverlaysVocab.VOCAB.get(MediaOverlaysVocab.PROPERTIES.DURATION));
     if (!totalDurationExpressions.isEmpty())
     {
@@ -471,7 +472,7 @@ public class OPFChecker30 extends OPFChecker
             totalDurationExpressions.iterator().next().getValue());
         // sum all the individual durations (non-primary metadata expressions)
         SmilClock sumDuration = new SmilClock();
-        Set<Metadata> allDurations = metadata
+        List<Metadata> allDurations = metadata
             .getAny(MediaOverlaysVocab.VOCAB.get(MediaOverlaysVocab.PROPERTIES.DURATION));
         for (Metadata durationExpression : allDurations)
         {
@@ -504,7 +505,7 @@ public class OPFChecker30 extends OPFChecker
           report.message(MessageId.NAV_003, EPUBLocation.of(context));
         }
         // Search a "dc:source" metadata expression
-        Set<Metadata> dcSourceMetas = ((OPFHandler30) opfHandler).getMetadata()
+        List<Metadata> dcSourceMetas = ((OPFHandler30) opfHandler).getMetadata()
             .getPrimary(DCMESVocab.VOCAB.get(DCMESVocab.PROPERTIES.SOURCE));
         if (dcSourceMetas.isEmpty())
         {
